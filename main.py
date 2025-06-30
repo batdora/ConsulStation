71663e6087304adce9074ee6369115eaa4e2ad6d
batdora <145152881+batdora@users.noreply.github.com>
2025-06-30 11:32:13 +0300

    Add/Learn pydantic BaseModel schema and implement POST using it

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# source venvFastAPI/bin/activate
-from typing import Union
-from typing import Any
+from typing import Optional, Union
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+# For optional requests, use a defaulted field; for full optional default to None
+# For str valued requests, if you use int; it converts int to str automatically. Look out.
 
 @app.get("/")
 def read_root():
@@ -13,10 +20,11 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post"}
+    return {"message": "this is your post"}
 
 @app.post("/createpost")
-def create_post(payLoad:dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"successfully created post with title: {payLoad['title']} & content: {payLoad['content']}"}
+def create_post(new_post:Post):
+    print(new_post.published)
+    new_post.dict()
+    return {"data":new_post}
 
