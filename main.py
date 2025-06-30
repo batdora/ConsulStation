8eda272d9f3593ea9d8857793fde610c046919cd
batdora <145152881+batdora@users.noreply.github.com>
2025-06-30 09:51:21 +0300

    Add/learn POST function with dict and Body()

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # source venvFastAPI/bin/activate
 from typing import Union
+from typing import Any
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message":"welcome to my url lol"}
+    return {"message": "welcome to my url lol"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"this is your post"}
+    return {"data": "this is your post"}
+
+@app.post("/createpost")
+def create_post(payLoad:dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"successfully created post with title: {payLoad['title']} & content: {payLoad['content']}"}
+
