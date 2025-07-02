f0ed1c6d4bd177d8760453a3207c0c04133f9948
batdora <145152881+batdora@users.noreply.github.com>
2025-07-02 15:01:10 +0300

    Comment out any route not using SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from typing import Optional, Union, Any
 from fastapi import FastAPI, Response, status, HTTPException, Depends, Query
-from sqlmodel import Session, SQLModel, select
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -9,12 +8,9 @@ from psycopg.rows import dict_row
 import time
 import signal
 import sys
-from sqlmodel import SQLModel
-from app.database import engine, SessionDep
+from app.database import engine, get_db
 import app.models as models
-from typing import Annotated
-
-SQLModel.metadata.create_all(bind=engine)
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -65,52 +61,54 @@ def find_post(id):
 def read_root():
     return {"message": "welcome to my url lol"}
 
+"""CRUD Operations using psycopg and SQL code"""
 # GET all posts
-@app.get("/posts")
-def get_posts():
-    cur.execute("""SELECT * FROM posts""")
-    posts= cur.fetchall()
-    return {"data": posts}
+#@app.get("/posts")
+#def get_posts():
+    #cur.execute("""SELECT * FROM posts""")
+    #posts= cur.fetchall()
+    #return {"data": posts}
 
 # CREATE a post
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post:Post):
-    cur.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
-    new_post = cur.fetchone()
-    conn.commit()
-    return {"data":new_post}
+#@app.post("/posts", status_code=status.HTTP_201_CREATED)
+#def create_post(post:Post):
+    #cur.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
+    #new_post = cur.fetchone()
+    #conn.commit()
+    #return {"data":new_post}
 
 # GET ID specific post
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cur.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),)) # type: ignore
-    post = cur.fetchone()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
-    return {"message": post}
+#@app.get("/posts/{id}")
+#def get_post(id: int):
+    #cur.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),)) # type: ignore
+    #post = cur.fetchone()
+    #if not post:
+    #    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
+    #return {"message": post}
 
 # DELETE Post
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cur.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),)) # type: ignore
-    post = cur.fetchone()
-    conn.commit()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
-    else:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
+#@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+#def delete_post(id: int):
+    #cur.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),)) # type: ignore
+    #post = cur.fetchone()
+    #conn.commit()
+    #if not post:
+    #    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
+    #else:
+    #    return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 # Update with PUT
-@app.put("/posts/{id}")
-def update_post_1(id: int, post: Post):
-    cur.execute("""UPDATE posts SET title= %s, content= %s, published= %s WHERE id = %s RETURNING *""", (post.title,post.content,str(post.published),str(id)))
-    updated_post = cur.fetchone() # type: ignore
-    conn.commit()
-    if not updated_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
-    return {"data":updated_post}
+#@app.put("/posts/{id}")
+#def update_post_1(id: int, post: Post):
+    #cur.execute("""UPDATE posts SET title= %s, content= %s, published= %s WHERE id = %s RETURNING *""", (post.title,post.content,str(post.published),str(id)))
+    #updated_post = cur.fetchone() # type: ignore
+    #conn.commit()
+    #if not updated_post:
+    #    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with the id: {id} was not found")
+    #return {"data":updated_post}
+
 
 @app.get("/sqlTest")
-def test(db: SessionDep):
+def test(db: Session = Depends(get_db)):
     posts= db.query(models.Post).all() # type: ignore
     return{"data":posts}
